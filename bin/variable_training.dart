8a70424a1c4312dc74a35afaecb6483a52dadcb1
TechieChuks <chuksanoliefo@gmail.com>
2024-12-25 00:24:31 -0800

    first init

---
+++ bin/variable_training.dart
@@ -0,0 +1,5 @@
+import 'package:variable_training/variable_training.dart' as variable_training;
+
+void main(List<String> arguments) {
+  print('Hello world: ${variable_training.calculate()}!');
+}
---
+++ test/variable_training_test.dart
@@ -0,0 +1,8 @@
+import 'package:variable_training/variable_training.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
